d0bf963cbb3fc77f028c177436526422da74d1f9
Malik4445 <abdulmalikenyoneibrahim@gmail.com>
2025-10-14 03:47:35 +0100

    updating postcss.config.js and added tailwindcss to implement css stylings

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js (Correct)
+export default {
+  plugins: {
+    // Use the correct plugin alias for Tailwind CSS in PostCSS
+    '@tailwindcss/postcss': {}, 
+    autoprefixer: {},
+  },
+};
